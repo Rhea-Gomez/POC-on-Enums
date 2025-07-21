f7c8322784a462e6c969f634de20d6a4302c1aa3
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-22 00:31:24 +0530

    Assignment on POC on Enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Enums
+{
+    internal class Program
+    {
+        public enum Season { WINTER, SPRING, SUMMER, FALL}
+        static void Main(string[] args)
+        {
+           int x = (int)Season.WINTER;
+            int y = (int)Season.SUMMER;
+            Console.WriteLine("WINTER = {0}", x);
+            Console.WriteLine("SUMMER = {0}", y);
+
+            //using GetNames() method
+            foreach(string s in Enum.GetNames(typeof(Season)))
+            {
+                Console.WriteLine(s);
+            }
+
+            //using GetValues() method
+            foreach(string s in Enum.GetValues(typeof(Season)))
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
